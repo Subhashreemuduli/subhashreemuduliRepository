b79ec62d4875347d553b4e292f36f2373c0bb31d
Subhashreemuduli <subhashreemuduli097@gmail.com>
2022-09-20 21:58:44 +0530

    String Problem2

---
+++ src/com/ivycomptech/stringproblem/StringChecking.java
@@ -0,0 +1,11 @@
+package com.ivycomptech.stringproblem;
+
+public class StringChecking {
+    public static void main(String[] args )
+    {
+        String s1="Stephen Edwin King";
+        String s2="Walter Winchell";
+        System.out.println(s1.equals(s2));
+        System.out.println(s1.equalsIgnoreCase(s1));
+    }
+}
